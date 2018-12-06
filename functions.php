490563923fe42e4bcce1750de611c34a5c69ec27
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-12-06 05:08:13 +0000

    Hiding single product page. Now add tickets to cart straight from shop only.

--- functions.php
+++ functions.php
@@ -211,3 +211,6 @@ function quantity_inputs_for_woocommerce_loop_add_to_cart_link( $html, $product 
 	}
 	return $html;
 }
+
+remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
+remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
